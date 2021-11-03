4ffd3b4cc4b93f8358005a84da906d335a540a60
Kim Beom Jin <planet1995@nate.com>
2021-11-03 21:16:12 +0900

    redux 툴킷 state 연결하기

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,4 @@
-import { createStore } from 'redux';
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 
 const initialState = {
   counter: 0,
@@ -7,7 +6,7 @@ const initialState = {
 };
 
 // 리덕스 툴킷 적용
-createSlice({
+const counterSlice = createSlice({
   name: 'counter',
   initialState,
   reducers: {
@@ -26,41 +25,9 @@ createSlice({
   },
 });
 
-// reducer 생성
-const counterReducer = (state = initialState, action) => {
-  if (action.type === 'increment') {
-    return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter, // 값 증가일 때 showCounter 설정 필요(왜냐하면 전체상태의 객체를 리턴해줘야 함)
-    };
-  }
+// 여러 리듀서를 하나로 병합하기 위함
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
 
-  if (action.type === 'increase') {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === 'decrement') {
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === 'toggle') {
-    return {
-      showCounter: !state.showCounter,
-      counter: state.counter,
-    };
-  }
-
-  return state;
-};
-
-// 스토어 생성
-const store = createStore(counterReducer);
-
-// store 내보내기
 export default store;
