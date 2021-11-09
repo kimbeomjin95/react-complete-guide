83aecd8a8c9752395c2dd9b3c4166e6cb0a9e0d5
kimbeomjin95 <kimbeomjin95@github.com>
2021-11-09 21:45:04 +0900

    auth slice 추가

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,15 @@
 import { createSlice, configureStore } from '@reduxjs/toolkit';
 
-const initialState = {
+const initialCounterState = {
   counter: 0,
   showCounter: true,
+  isAuthenticated: false,
 };
 
 // 리덕스 툴킷 적용
 const counterSlice = createSlice({
   name: 'counter',
-  initialState,
+  initialState: initialCounterState, // counter 전용
   reducers: {
     increment(state) {
       state.counter++;
@@ -25,11 +26,32 @@ const counterSlice = createSlice({
   },
 });
 
-// 여러 리듀서를 하나로 병합하기 위함
+const initialAuthState = {
+  isAuthenticated: false,
+};
+
+// 인증 전용 슬라이스
+const authSlice = createSlice({
+  name: 'authentication',
+  initialState: initialAuthState,
+  // 리듀서 등록(상태변경)
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    }, // state: 현 상태값
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+// 여러 리듀서를 하나로 병합하기 위함(자동병합
 const store = configureStore({
-  reducer: counterSlice.reducer,
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
 });
 
 // counter action create function
 export const countAction = counterSlice.actions;
+export const authAction = authSlice.actions;
+
 export default store;
