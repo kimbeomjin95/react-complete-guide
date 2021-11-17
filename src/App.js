1cbde5dccc1eb66078e667b9efc85fd343d69802
kimbeomjin95 <kimbeomjin95@github.com>
2021-11-17 20:53:09 +0900

    Section3: 리액트 기초 및 실습 컴포넌트 초기 셋팅

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import React, { Fragment }from 'react'
-import Counter from "./components/Counter";
-import "./components/Counter.module.css";
-import Header from "./components/Header";
-import "./components/Header.module.css";
-import Auth from "./components/Auth";
-import "./components/Auth.module.css";
+import React, { Fragment } from 'react';
+import Counter from './components/Counter';
+import './components/Counter.module.css';
+import Header from './components/Header';
+import './components/Header.module.css';
+import Auth from './components/Auth';
+import './components/Auth.module.css';
 
 function App() {
   return (
-    <Fragment>
-      <Header />
-      <Auth />
-      <Counter/>
-    </Fragment>
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
   );
 }
 
