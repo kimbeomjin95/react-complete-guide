22f70ba9ce4372b8532957df831ccde1bf58b3ab
kimbeomjin95 <kimbeomjin95@github.com>
2021-11-17 21:19:38 +0900

    컴포넌트를 여러 컴포넌트로 분할하기

--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -1,18 +1,12 @@
 import './ExpenseItem.css';
+import ExpensiveDate from './ExpenseDate';
+import React from 'react';
 
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString('en-US', { month: 'long' });
-  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
-  const year = props.date.getFullYear();
-
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
       <div className="expense-item__description">
+        <ExpensiveDate date={props.date} />
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
