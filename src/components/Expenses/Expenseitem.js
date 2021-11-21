c2009019b2f5e98e0fb1302484006f8b257922c0
kimbeomjin95 <kimbeomjin95@github.com>
2021-11-21 14:50:51 +0900

    44. 대체 함수 문법(syntax, arrow function)

--- src/components/Expenses/Expenseitem.js
+++ src/components/Expenses/Expenseitem.js
@@ -3,7 +3,7 @@ import ExpensiveDate from './ExpenseDate';
 import React from 'react';
 import Card from '../UI/Card';
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
   return (
     <Card className="expense-item">
       <div className="expense-item__description">
@@ -13,6 +13,6 @@ function ExpenseItem(props) {
       </div>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
