13c97866f75ade499f6155806a09875c5177a5e1
kimbeomjin95 <kimbeomjin95@github.com>
2021-11-21 15:19:53 +0900

    47. 이벤트 리스닝 및 이벤트 핸들러 수행하기

--- src/components/Expenses/Expenseitem.js
+++ src/components/Expenses/Expenseitem.js
@@ -4,12 +4,17 @@ import React from 'react';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log('clicked!!!');
+  };
+
   return (
     <Card className="expense-item">
       <div className="expense-item__description">
         <ExpensiveDate date={props.date} />
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
+        <button onClick={clickHandler}>Change Title</button>
       </div>
     </Card>
   );
