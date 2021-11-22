9cd6792d49a9daebc0840cf309f9a371820e6ebe
kimbeomjin95 <kimbeomjin95@github.com>
2021-11-22 21:33:43 +0900

    62. 연습하기 : 이벤트 및 State 작업하기 (정답)

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,13 +1,28 @@
 import ExpenseItem from './Expenseitem';
-import React from 'react';
+import React, { useState } from 'react';
 import './Expenses.css';
 import Card from '../UI/Card';
+import ExpenseFilter from './ExpenseFilter';
 
-const Expenses = (props) => {
-  const { items } = props;
+const Expenses = ({ items }) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
 
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  /*
+   * TODO
+   * 1) ExpenseFilter에서 Expense 컴포넌트로 state 올리기
+   * 2) Expense 컴포넌트안에서 state로 저장
+   * 3) 변경된 이벤트를 수신하고 데이터를 더 높은 레벨의 컴포넌트로 포워드 필요.
+   * */
   return (
     <Card className="expenses">
+      <ExpenseFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
       <ExpenseItem
         title={items[0].title}
         amount={items[0].amount}
